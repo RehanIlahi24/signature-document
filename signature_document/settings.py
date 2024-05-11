359410478858bf872fc39a47c4e2e73b78c0af35
RehanIlahi24 <rehanilahi680@gmail.com>
2024-05-12 01:37:13 +0500

    commit

--- signature_document/settings.py
+++ signature_document/settings.py
@@ -133,3 +133,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL='login'
+
+SECURE_SSL_REDIRECT = True
+CORS_ALLOWED_ORIGINS = [
+    'https://dysign.dyrevolution.com',
+]
