5c251ffa040a5f852a10d5dea47bd3aa9dd5e6f9
RehanIlahi24 <rehanilahi680@gmail.com>
2024-05-17 00:40:33 +0500

    commit

--- signature_document/settings.py
+++ signature_document/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rehanilahi680@gmail.com'
-EMAIL_HOST_PASSWORD = 'developer840444'
+EMAIL_HOST_USER = 'dysigndyrevolution@gmail.com'
+EMAIL_HOST_PASSWORD = 'dysign123'
