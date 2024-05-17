8f2ce3f416ed8c1c577206609cdebca4bf6e60de
RehanIlahi24 <rehanilahi680@gmail.com>
2024-05-17 21:51:00 +0500

    commit

--- mainapp/utils.py
+++ mainapp/utils.py
@@ -4,7 +4,7 @@ from sendgrid.helpers.mail import Mail
 import os
 from django.conf import settings
 from django.core.mail import send_mail
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 def pagination_custom(request,table):
     paginator = Paginator(table, 10)
@@ -17,13 +17,13 @@ def pagination_custom(request,table):
         table = paginator.page(paginator.num_pages)
     return table
 
-# load_dotenv() 
+load_dotenv() 
 apikey = os.getenv("SENDGRID_APIKEY")
 
 def send_email(to_email,subject,html):
     print("email called before")
     message = Mail(
-        from_email='rehanilahi680@gmail.com',
+        from_email='dysignca@gmail.com',
         to_emails=to_email,
         subject=subject,
         plain_text_content=html
@@ -39,15 +39,15 @@ def send_email(to_email,subject,html):
     except Exception as e:
         print('error : ', e.message)
 
-def send_email_siging(email, subject_content, message_content):
-    print('start')
-    subject = subject_content
-    message = message_content
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = [email,]
-    try:
-        send_mail(subject, message, from_email, recipient_list)
-        print('Email sent successfully')
-    except Exception as e:
-        print('An error occurred while sending the email:', e)
-    print('end')
+# def send_email_siging(email, subject_content, message_content):
+#     print('start')
+#     subject = subject_content
+#     message = message_content
+#     from_email = settings.EMAIL_HOST_USER
+#     recipient_list = [email,]
+#     try:
+#         send_mail(subject, message, from_email, recipient_list)
+#         print('Email sent successfully')
+#     except Exception as e:
+#         print('An error occurred while sending the email:', e)
+#     print('end')
