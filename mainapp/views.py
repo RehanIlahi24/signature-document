6151216a8874365a9560466d3eafcea2f4254e88
RehanIlahi24 <rehanilahi680@gmail.com>
2024-05-26 16:24:11 +0500

    commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -520,7 +520,7 @@ def signed_document_detail(request, id):
         if request.user.is_superuser:
             document_ob = Document.objects.get(id=id)
         else:
-            document_ob = Document.objects.filter(id=id, user=request.user)
+            document_ob = Document.objects.filter(id=id).first()
         # elif request.user.is_superuser:
         return render(request, 'signed_document_detail.html', {'doc' : document_ob})
     except:
