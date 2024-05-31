7907954b642298b29697ce69d025850d7dc93162
RehanIlahi24 <rehanilahi680@gmail.com>
2024-05-31 21:23:21 +0500

    commit

--- mainapp/utils.py
+++ mainapp/utils.py
@@ -22,11 +22,14 @@ load_dotenv()
 apikey = os.getenv("SENDGRID_APIKEY")
 
 def send_email_siging(email, subject_content, message_content):
+    print('send email function start')
     subject = subject_content
     message = message_content
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [email,]
     try:
         send_mail(subject, message, from_email, recipient_list)
+        print('send email function success')
     except Exception as e:
         print('An error occurred while sending the email:', e)
+    print('send email function end')
